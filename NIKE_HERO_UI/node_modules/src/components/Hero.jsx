f2316869d772b8f99e9e65c3bb8ff757a2720306
Mohammad Nandurbarwala <mohammdali0326@gmail.com>
2025-02-20 09:38:07 +0530

    added functionality to the back button, added responsiveness to login, home and player pages

---
+++ NIKE_HERO_UI/node_modules/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+const Hero = () => {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVES THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+        <div className="shopping">
+          <p>Also Available on</p>
+          <div className="brand-icons">
+            <img src="/public/images/amazon.png" alt="Amazon-logo" />
+            <img src="/public/images/flipkart.png" alt="Flipcart-logo" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/public/images/shoe_image.png" alt="shoe-image" />
+      </div>
+    </main>
+  );
+};
+export default Hero;
